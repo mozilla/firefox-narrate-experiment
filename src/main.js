0cb7380b80176f96a884a4c61ba3e2ec967fcac1
Irakli Gozalishvili <rfobic@gmail.com>
2018-09-04 11:08:45 -0700

    Turns out we don't need aboutScripts
    After talking to @rpl on IRC about the exception we worked around with aboutScripts he pointed out that contenScripts defined in manifest get added through diffecent code path and it turns out mozillaAddons permission was all we needed.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 const iconURL = browser.extension.getURL("listen-with-label.svg")
-const scriptURL = browser.extension.getURL("src/content-script.js")
 
 const setIcon = async () => {
   await browser.readerIcon.change(iconURL, 52)
 }
 
 browser.windows.onCreated.addListener(setIcon)
-browser.aboutScripts.register({
-  matches: ["about:reader*"],
-  js: [{ file: scriptURL }]
-})
-
 setIcon()
