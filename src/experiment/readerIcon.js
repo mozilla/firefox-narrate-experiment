02b86e6d3460fddf28f54304ac8f6e8db860d68a
Irakli Gozalishvili <rfobic@gmail.com>
2018-10-12 11:53:37 -0700

    Update implementation to show title + description as in design.

--- src/experiment/readerIcon.js
+++ src/experiment/readerIcon.js
@@ -5,6 +5,8 @@
   const ARCHOR_ID = "-notification-icon"
   const ID = "reader-mode-button"
   const POPUP_ID = "narrate-experiment-doorhanger"
+  const PRIMARY_ACTION = "narrate-experiment-primary-action"
+  const SECONDARY_ACTION = "narrate-experiment-secondary-action"
 
   const { ExtensionParent } = Cu.import(
     "resource://gre/modules/ExtensionParent.jsm",
@@ -27,14 +29,14 @@
         Services.mm.addMessageListener("Reader:UpdateReaderButton", this)
       }
 
-      windowTracker.addOpenListener(this.onWindowOpened)
+      windowTracker.addOpenListener(this.onOpenWindow)
 
       for (const window of windowTracker.browserWindows()) {
         this.onOpenWindow(window)
       }
     }
     exit(reason) {
-      windowTracker.removeOpenListener(this.onWindowOpened)
+      windowTracker.removeOpenListener(this.onOpenWindow)
       if (this.config.popup) {
         Services.mm.removeMessageListener("Reader:UpdateReaderButton", this)
       }
@@ -43,18 +45,35 @@
         this.resetWindow(window)
       }
     }
-    updateIcon(window) {
-      const { iconURL, iconWidth } = this.config
-      const icon = window.document.getElementById(ID)
-      icon.style.listStyleImage = `url(${iconURL})`
-      icon.style.width = `${iconWidth}px`
+    onOpenWindow({ document }) {
+      patchIcon(document, this.config)
+      if (this.config.popup) {
+        this.addPopupListeners(document)
+      }
     }
-    resetIcon(window) {
-      const icon = window.document.getElementById("reader-mode-button")
-      icon.removeAttribute("style")
+    resetWindow({ document }) {
+      resetIcon(document)
+      if (this.config.popup) {
+        removePopupView(document)
+        this.removePopupListeners(document)
+      }
     }
-    onOpenWindow(window) {
-      this.updateIcon(window)
+    addPopupListeners(document) {
+      document.addEventListener(PRIMARY_ACTION, this)
+      document.addEventListener(SECONDARY_ACTION, this)
+    }
+    removePopupListeners(document) {
+      document.removeEventListener(PRIMARY_ACTION, this)
+      document.removeEventListener(SECONDARY_ACTION, this)
+    }
+
+    handleEvent(event) {
+      switch (event.type) {
+        case PRIMARY_ACTION:
+          return this.onPrimaryAction(event.target)
+        case SECONDARY_ACTION:
+          return this.onSecondaryAction(event.target)
+      }
     }
     receiveMessage(message) {
       switch (message.name) {
@@ -63,107 +82,118 @@
         }
       }
     }
+
+    onPrimaryAction(document) {
+      const button = getReaderButton(document)
+      if (button) {
+        button.click()
+      }
+    }
+    onSecondaryAction(document) {
+      hidePopup(document)
+    }
     onReaderButtonUpdate({ target, data }) {
-      if (data.isArticle) {
-        this.showPopup(target)
-      }
-    }
-    setPopupAnchor(browser, anchor) {
-      browser[`${ID}${ANCHOR_SUFFIX}`] = anchor
-    }
-    resetPopupAnchor(browser) {
-      delete browser[`${ID}${ANCHOR_SUFFIX}`]
-    }
-    showPopup(browser) {
-      const { popup } = this.config
-      const document = browser.ownerDocument
-      const { PopupNotifications } = document.defaultView
-      const anchor = document.getElementById(ID)
-      if (anchor) {
-        this.setPopupAnchor(browser, anchor)
-        const ui = PopupNotifications.show(
-          browser,
-          POPUP_ID,
-          "<> Firefox can read you any page on the internet.",
-          ID,
-          {
-            label: "Listen to this Article",
-            accessKey: "D",
-            callback() {
-              anchor.click()
-            }
-          },
-          [
-            {
-              label: "Not Now",
-              accessKey: "K",
-              callback() {
-                ui.remove()
-              }
-            }
-          ],
-          {
-            name: "Want to listen instead ?",
-            popupIconURL: popup.iconURL,
-            dismissed: false,
-            hideClose: true
-          }
-        )
-      }
-    }
-    resetWindow(window) {
-      this.resetIcon(window)
-      if (this.config.popup) {
-        const browser = window.gBrowser.selectedBrowser
-        this.resetPopupAnchor(browser)
+      if (data && data.isArticle) {
+        showPopup(target.ownerDocument, this.config.popup)
+      } else {
+        hidePopup(target.ownerDocument)
       }
     }
   }
 
+  const patchIcon = (document, { iconURL, iconWidth }) => {
+    const icon = getReaderButton(document)
+    icon.style.listStyleImage = `url(${iconURL})`
+    icon.style.width = `${iconWidth}px`
+  }
+
+  const resetIcon = document => {
+    const icon = getReaderButton(document)
+    if (icon) {
+      icon.removeAttribute("style")
+    }
+  }
+
+  const hidePopup = async document => {
+    const panel = getPopupView(document)
+    if (panel) {
+      await document.defaultView.PanelMultiView.hidePopup(panel)
+    }
+  }
+
+  const showPopup = async (document, popup) => {
+    const panel = getPopupView(document) || makePopupView(document, popup)
+    const button = getReaderButton(document)
+    await document.defaultView.PanelMultiView.openPopup(
+      panel,
+      button,
+      "bottomcenter topleft"
+    )
+  }
+
+  const getReaderButton = document => document.getElementById(ID)
+  const getPopupView = document => document.getElementById(POPUP_ID)
+
+  const removePopupView = document => {
+    const view = getPopupView(document)
+    if (view) {
+      view.remove()
+    }
+  }
+
+  const makePopupView = (document, popup) => {
+    var panel = document.createXULElement("panel")
+    panel.classList.add("popup-notification-panel")
+    panel.setAttribute("id", POPUP_ID)
+    panel.setAttribute("followanchor", true)
+    panel.setAttribute("type", "arrow")
+    // panel.setAttribute("noautohide", true)
+
+    const notification = panel.appendChild(
+      document.createXULElement("popupnotification")
+    )
+    notification.setAttribute("name", popup.title)
+    notification.setAttribute("orient", "vertical")
+
+    notification.setAttribute("buttonlabel", popup.primaryButtonLabel)
+    notification.setAttribute("buttonhighlight", true)
+    notification.setAttribute(
+      "buttoncommand",
+      `document.dispatchEvent(new CustomEvent("${PRIMARY_ACTION}"))`
+    )
+
+    notification.setAttribute("icon", popup.iconURL)
+    notification.setAttribute("closebuttonhidden", "true")
+    notification.setAttribute("checkboxhidden", "true")
+
+    notification.setAttribute(
+      "secondarybuttonlabel",
+      popup.secondaryButtonLabel
+    )
+    notification.setAttribute("dropmarkerhidden", "true")
+    notification.setAttribute(
+      "secondarybuttoncommand",
+      `document.dispatchEvent(new CustomEvent("${SECONDARY_ACTION}"))`
+    )
+
+    const content = notification.appendChild(
+      document.createXULElement("popupnotificationcontent")
+    )
+    content.setAttribute("orient", "vertical")
+
+    const description = content.appendChild(
+      document.createXULElement("description")
+    )
+    description.textContent = popup.description
+
+    return document.querySelector("#mainPopupSet").appendChild(panel)
+  }
   this.readerIcon = class readerIcon extends ExtensionAPI {
     getAPI(context) {
       return {
         readerIcon: {
           activate: async config => {
             this.actor = NarrateActor.spawn(config)
-          },
-          showDoorHanger: async iconURL => {
-            for (const browser of browsers()) {
-              const anchor = browser.document.getElementById(ID)
-              const selectedBrowser = browser.gBrowser.selectedBrowser
-
-              if (anchor) {
-                selectedBrowser[`${ID}${ANCHOR_SUFFIX}`] = anchor
-
-                const notification = window.PopupNotifications.show(
-                  browser,
-                  POPUP_ID,
-                  "Need to multi-task or just rest your eyes? Firefox can read webpages to you.",
-                  ID,
-                  {
-                    label: "Listen to this Article",
-                    accessKey: "D",
-                    callback() {
-                      browser.document.getElementById(ID).click()
-                    }
-                  },
-                  [
-                    {
-                      label: "Not Now",
-                      accessKey: "K",
-                      callback() {
-                        notification.remove()
-                      }
-                    }
-                  ],
-                  {
-                    popupIconURL: iconURL,
-                    persistWhileVisible: false,
-                    dismissed: false
-                  }
-                )
-              }
-            }
           }
         }
       }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ browser.readerIcon.activate({
   iconURL: browser.extension.getURL("listen-with-label.svg"),
   iconWidth: 52,
   popup: {
-    iconURL: browser.extension.getURL("listen-without-label.svg")
+    iconURL: browser.extension.getURL("listen-without-label.svg"),
+    title: "Want to listen instead ?",
+    description: "Firefox can read you any page on the internet.",
+    primaryButtonLabel: "Listen to this Article",
+    secondaryButtonLabel: "Not Now"
   }
 })
