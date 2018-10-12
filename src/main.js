5673866b483e0cf50a7da3725794a0488792014e
Irakli Gozalishvili <rfobic@gmail.com>
2018-10-12 16:18:04 -0700

    Update icons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 browser.narrate.activate({
   iconURL: browser.extension.getURL("listen-with-label.svg"),
-  iconWidth: 52,
+  iconWidth: 50,
   popup: {
-    iconURL: browser.extension.getURL("listen-without-label.svg"),
+    iconURL: browser.extension.getURL("listen-without-label-grey.svg"),
     title: "Want to listen instead ?",
     description: "Firefox can read you any page on the internet.",
     primaryButtonLabel: "Listen to this Article",
